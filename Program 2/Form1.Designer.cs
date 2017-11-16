550e64877b28e935c6119165fc2e3a03583166b3
Tony Piccirilli <anpicc01@louisville.edu>
2017-11-16 08:31:09 -0500

    Initial commit

---
+++ Program 2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameAnswer = new System.Windows.Forms.TextBox();
+            this.creditHoursLabel = new System.Windows.Forms.Label();
+            this.creditHourAnswer = new System.Windows.Forms.TextBox();
+            this.timeButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.dateOutput = new System.Windows.Forms.Label();
+            this.timeOutput = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(12, 28);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(181, 13);
+            this.lastNameLabel.TabIndex = 0;
+            this.lastNameLabel.Text = "Write the first letter of your last name.";
+            // 
+            // lastNameAnswer
+            // 
+            this.lastNameAnswer.Location = new System.Drawing.Point(15, 44);
+            this.lastNameAnswer.Name = "lastNameAnswer";
+            this.lastNameAnswer.Size = new System.Drawing.Size(100, 20);
+            this.lastNameAnswer.TabIndex = 1;
+            // 
+            // creditHoursLabel
+            // 
+            this.creditHoursLabel.AutoSize = true;
+            this.creditHoursLabel.Location = new System.Drawing.Point(12, 83);
+            this.creditHoursLabel.Name = "creditHoursLabel";
+            this.creditHoursLabel.Size = new System.Drawing.Size(198, 13);
+            this.creditHoursLabel.TabIndex = 2;
+            this.creditHoursLabel.Text = "How many credit hours have you taken?";
+            // 
+            // creditHourAnswer
+            // 
+            this.creditHourAnswer.Location = new System.Drawing.Point(15, 99);
+            this.creditHourAnswer.Name = "creditHourAnswer";
+            this.creditHourAnswer.Size = new System.Drawing.Size(100, 20);
+            this.creditHourAnswer.TabIndex = 3;
+            // 
+            // timeButton
+            // 
+            this.timeButton.Location = new System.Drawing.Point(3, 173);
+            this.timeButton.Name = "timeButton";
+            this.timeButton.Size = new System.Drawing.Size(144, 23);
+            this.timeButton.TabIndex = 4;
+            this.timeButton.Text = "Registration Time";
+            this.timeButton.UseVisualStyleBackColor = true;
+            this.timeButton.Click += new System.EventHandler(this.timeButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(153, 173);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(234, 173);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // dateOutput
+            // 
+            this.dateOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dateOutput.Location = new System.Drawing.Point(314, 41);
+            this.dateOutput.Name = "dateOutput";
+            this.dateOutput.Size = new System.Drawing.Size(119, 23);
+            this.dateOutput.TabIndex = 7;
+            this.dateOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timeOutput
+            // 
+            this.timeOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeOutput.Location = new System.Drawing.Point(314, 96);
+            this.timeOutput.Name = "timeOutput";
+            this.timeOutput.Size = new System.Drawing.Size(119, 23);
+            this.timeOutput.TabIndex = 8;
+            this.timeOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(311, 28);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(89, 13);
+            this.dateLabel.TabIndex = 9;
+            this.dateLabel.Text = "Registration Date";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(311, 83);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(89, 13);
+            this.timeLabel.TabIndex = 10;
+            this.timeLabel.Text = "Registration Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 246);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.timeOutput);
+            this.Controls.Add(this.dateOutput);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.timeButton);
+            this.Controls.Add(this.creditHourAnswer);
+            this.Controls.Add(this.creditHoursLabel);
+            this.Controls.Add(this.lastNameAnswer);
+            this.Controls.Add(this.lastNameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameAnswer;
+        private System.Windows.Forms.Label creditHoursLabel;
+        private System.Windows.Forms.TextBox creditHourAnswer;
+        private System.Windows.Forms.Button timeButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label dateOutput;
+        private System.Windows.Forms.Label timeOutput;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ Program 2/Form1.cs
@@ -0,0 +1,318 @@
+﻿// Tony Piccirilli
+// CIS 199-01-4162
+// Program 2
+// March 6, 2016
+// This program will take a student's first letter of their last name
+// and what their credit hours are and it will give them
+// the earliest day and time the can begin registering 
+// for classes next year.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare constants
+                const double SOPHOMORE_HOURS = 30;
+                const double JUNIOR_HOURS = 60;
+                const double SENIOR_HOURS = 90;
+                const string FRESHMAN_DAY1 = "Tuesday, April 5";
+                const string FRESHMAN_DAY2 = "Wednesday, April 6";
+                const string SOPHOMORE_DAY1 = "Friday, April 1";
+                const string SOPHOMORE_DAY2 = "Monday, April 4";
+                const string JUNIOR_DAY = "Thursday, March 31";
+                const string SENIOR_DAY = "Wednesday, March 30";
+                const string TIME1 = "8:30 a.m.";
+                const string TIME2 = "10 a.m.";
+                const string TIME3 = "11:30 a.m.";
+                const string TIME4 = "2:00 p.m.";
+                const string TIME5 = "4:00 p.m";
+
+                // Declare local variables
+                string lastName;
+                double creditHours;
+                
+                // Find the student's last name.
+                lastName = lastNameAnswer.Text;
+                lastName = lastName.ToUpper();
+               
+                // Validate the credit hours entered.
+                if (double.TryParse(creditHourAnswer.Text, out creditHours))
+                {
+                    if (creditHours < 0)
+                    {
+                        MessageBox.Show("The number you entered is invaild");
+                    }
+                }
+                    
+                    // Determine what registration time is for student's with at least 90 hours. 
+                if (creditHours >= SENIOR_HOURS)
+                {
+                    dateOutput.Text = SENIOR_DAY;
+
+                    if (lastName == "A" || lastName == "B" || lastName == "C" || lastName == "D")
+                    {
+                        timeOutput.Text = TIME5;
+                    }
+
+                    if (lastName == "E" || lastName == "F" || lastName == "G" || lastName == "H" || lastName == "I")
+                    {
+                        timeOutput.Text = TIME1;
+
+                    }
+                    if (lastName == "J" || lastName == "K" || lastName == "L" || lastName == "M" || lastName == "N" || lastName == "O")
+                    {
+
+                        timeOutput.Text = TIME2;
+
+                    }
+                    if (lastName == "P" || lastName == "Q" || lastName == "R" || lastName == "S")
+                    {
+
+                        timeOutput.Text = TIME3;
+
+                    }
+                    if (lastName == "T" || lastName == "U" || lastName == "V" || lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z")
+                    {
+
+                        timeOutput.Text = TIME4;
+
+                    }
+                }
+
+                    // Determine what registration time is for students with less than 90 hours but at least 60 hours.
+                if (creditHours >= JUNIOR_HOURS && creditHours < SENIOR_HOURS) 
+                {
+                    dateOutput.Text = JUNIOR_DAY;
+                    
+                    if (lastName == "A" || lastName == "B" || lastName == "C" || lastName == "D")
+                    {
+
+                        timeOutput.Text = TIME5;
+
+                    }
+                    if (lastName == "E" || lastName == "F" || lastName == "G" || lastName == "H" || lastName == "I")
+                    {
+
+                        timeOutput.Text = TIME1;
+
+                    }
+                    if (lastName == "J" || lastName == "K" || lastName == "L" || lastName == "M" || lastName == "N" || lastName == "O")
+                    {
+                        
+                        timeOutput.Text = TIME2;
+
+                    }
+                    if (lastName == "P" || lastName == "Q" || lastName == "R" || lastName == "S")
+                    {
+                        
+                        timeOutput.Text = TIME3;
+
+                    }
+                    if (lastName == "T" || lastName == "U" || lastName == "V" || lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z")
+                    {
+
+                        timeOutput.Text = TIME4;
+
+                    }
+                }
+                    // Display the Registration time for student's with less than 60 hours but at least 30 hours.
+
+                if ((creditHours >= SOPHOMORE_HOURS && creditHours < JUNIOR_HOURS))
+                {
+
+                    if (lastName == "A" || lastName == "B")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY2;
+                        timeOutput.Text = TIME4;
+
+                    }
+                    if (lastName == "C" || lastName == "D")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY2;
+                        timeOutput.Text = TIME5;
+
+                    }
+
+                    if (lastName == "E" || lastName == "F")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY1;
+                        timeOutput.Text = TIME1;
+
+                    }
+                    if (lastName == "G" || lastName == "H" || lastName == "I")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY1;
+                        timeOutput.Text = TIME2;
+
+                    }
+                    if (lastName == "J" || lastName == "K" || lastName == "L")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY1;
+                        timeOutput.Text = TIME3;
+
+                    }
+                    if (lastName == "M" || lastName == "N" || lastName == "O")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY1;
+                        timeOutput.Text = TIME4;
+
+                    }
+                    if (lastName == "P" || lastName == "Q")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY1;
+                        timeOutput.Text = TIME5;
+
+                    }
+                    if (lastName == "R" || lastName == "S")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY2;
+                        timeOutput.Text = TIME1;
+
+                    }
+                    if (lastName == "T" || lastName == "U" || lastName == "V")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY2;
+                        timeOutput.Text = TIME2;
+
+                    }
+                    if (lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z")
+                    {
+
+                        dateOutput.Text = SOPHOMORE_DAY2;
+                        timeOutput.Text = TIME3;
+
+                    }
+                }
+
+                    // Display the Registration time for students with less than 30 hours.
+
+                if (creditHours < SOPHOMORE_HOURS)
+                {
+                    if (lastName == "A" || lastName == "B")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY2;
+                        timeOutput.Text = TIME4;
+
+                    }
+                    if (lastName == "C" || lastName == "D")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY2;
+                        timeOutput.Text = TIME5;
+
+                    }
+                    if (lastName == "E" || lastName == "F")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY1;
+                        timeOutput.Text = TIME1;
+
+                    }
+                    if (lastName == "G" || lastName == "H" || lastName == "I")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY1;
+                        timeOutput.Text = TIME2;
+
+                    }
+                    if (lastName == "J" || lastName == "K" || lastName == "L")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY1;
+                        timeOutput.Text = TIME3;
+
+                    }
+                    if (lastName == "M" || lastName == "N" || lastName == "O")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY1;
+                        timeOutput.Text = TIME4;
+
+                    }
+                    if (lastName == "P" || lastName == "Q")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY1;
+                        timeOutput.Text = TIME5;
+
+                    }
+                    if (lastName == "R" || lastName == "S")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY2;
+                        timeOutput.Text = TIME1;
+
+                    }
+                    if (lastName == "T" || lastName == "U" || lastName == "V")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY2;
+                        timeOutput.Text = TIME2;
+
+                    }
+                    if (lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z")
+                    {
+
+                        dateOutput.Text = FRESHMAN_DAY2;
+                        timeOutput.Text = TIME3;
+
+                    }
+                }
+                    
+                    
+
+                    
+            }
+            catch
+            {
+            }
+
+
+                    
+            
+      }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the form for another student
+            lastNameAnswer.Text = "";
+            creditHourAnswer.Text = "";
+            dateOutput.Text = "";
+            timeOutput.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+
+    }
+}
